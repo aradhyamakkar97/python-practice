040bf6288141e2a3cc5b91a149411a4648d0a199
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-21 02:42:23 +0530

    using generators now in reverse.py

--- reverse.py
+++ reverse.py
@@ -1,17 +1,6 @@
-class Reverse:
-    
-    def __init__(self,data):
-        self.data = data
-        self.index = len(data)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index == 0:
-            raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
+def Reverse(data):
+    for index in range(len(data)-1,-1,-1):
+        yield data[index]
 
 def Main():
     rev = Reverse('Aradhya')
