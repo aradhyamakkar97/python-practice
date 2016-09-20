c47edac6e1bb4d124222985955b84646db96d5d4
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-21 03:12:23 +0530

    modulating code, add mod.py

---
+++ mod.py
@@ -0,0 +1,13 @@
+import vector2d
+
+def Main():
+    vec1 = vector2d.Vector2D(5,6)
+    vec2 = vector2d.Vector2D(1,1)
+
+    print(vec1.x)
+    print(vec1.y)
+
+    print(vec1)
+
+if __name__ == '__main__':
+    Main()
