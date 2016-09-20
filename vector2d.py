6f191000023fdd3b0f9f5b98ae864933388aef49
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-20 20:56:05 +0530

    same thing but using init now

--- vector2d.py
+++ vector2d.py
@@ -1,14 +1,11 @@
 class Vector2D:
-    x = 0.0
-    y = 0.0
 
-    def Set(self,x,y):
+    def __init__(self,x,y):
         self.x = x
         self.y = y
 
 def Main():
-    vec = Vector2D()
-    vec.Set(5,6)
+    vec = Vector2D(5,6)
     print("X: "+str(vec.x)+",Y: "+str(vec.y))
 
 if __name__=='__main__':
