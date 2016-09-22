dacd147b848446752400187b6b0b3b2805ac16a1
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-23 03:14:46 +0530

    optional arguments in fibn.py

--- advanced_topics/fibn.py
+++ advanced_topics/fibn.py
@@ -9,12 +9,18 @@ def fib(n):
 def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument("num",help="The Fibonachhhhhi num u wich to calculate ",type=int)
+    parser.add_argument("-o","--output",help="Output result to the file",action="store_true")
 
     args = parser.parse_args()
 
     result = fib(args.num)
 
     print ('the '+str(args.num)+"th fib number is "+str(result))
+
+    if args.output:
+        f = open("fibonachi.txt","a")
+        f.write(str(result)+'\n')
+        f.close()
 
 if __name__ == '__main__':
     Main()
