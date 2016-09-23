151664ada9b3e706b9008fada6eb642729dc6ee1
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-23 19:42:48 +0530

    add locking to timer.py

--- advanced_topics/timer.py
+++ advanced_topics/timer.py
@@ -1,17 +1,23 @@
-from threading import Thread
+import threading
 import time
+
+tLock = threading.Lock()
 
 def timer(name,delay,repeat):
     print("Timer: "+name+" Started")
+    tLock.acquire()
+    print(name+ " has aquired the lock")
     while repeat > 0:
         time.sleep(delay)
         print(name+" : "+str(time.ctime(time.time())))
         repeat -= 1
+    print(name+" is releasing the lock")
+    tLock.release()
     print("Timer: "+name+" Completed")
 
 def Main():
-    t1 = Thread(target=timer,args=("Timer1",1,5))
-    t2 = Thread(target=timer,args=("Timer2",2,5))
+    t1 = threading.Thread(target=timer,args=("Timer1",1,5))
+    t2 = threading.Thread(target=timer,args=("Timer2",2,5))
     t1.start()
     t2.start()
 
