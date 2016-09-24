73f21f702c1a9c0d63a58b9bae1368fa8ba3b9e4
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-24 23:44:40 +0530

    client server connection - basics

--- advanced_topics/tcpServer.py
+++ advanced_topics/tcpServer.py
@@ -2,7 +2,7 @@ import socket
 
 def Main():
     host = '127.0.0.1'
-    port = 5000
+    port = 4999
     s=socket.socket()
     s.bind((host,port))
 
@@ -11,7 +11,7 @@ def Main():
     print('connection from : '+str(addr))
 
     while True:
-        data = c.revc(1024).decode('utf-8')
+        data = c.recv(1024).decode('utf-8')
         if not data:
             break
         print("from connected user: "+data)
