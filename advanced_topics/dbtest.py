5a86e4569272487c2242f51a33829158af82dad8
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-28 03:56:03 +0530

    executing sql queries - dbtest.py

--- advanced_topics/dbtest.py
+++ advanced_topics/dbtest.py
@@ -1,13 +1,32 @@
 import sqlite3
 
 def Main():
-    con = sqlite3.connect('test.db')
-    cur = con.cursor()
-    cur.execute('SELECT SQLITE_VERSION()')
+    try:
 
-    data = cur.fetchone()
+        con = sqlite3.connect('test.db')
+        cur = con.cursor()
+        cur.execute('CREATE TABLE Pets(Id INT,Name TEXT,Price INT)')
+        cur.execute('INSERT INTO Pets VALUES(1,"Cat",400)')
+        cur.execute('INSERT INTO Pets VALUES(2,"Dog",456)')
+        cur.execute('INSERT INTO Pets VALUES(3,"Kutta",10000)')
 
-    print(data)
+        con.commit()
+
+        cur.execute('SELECT * FROM Pets')
+
+        data = cur.fetchall()
+        for row in data:
+            print(row)
+
+    except sqlite3.Error:
+        if con:
+            print("Error!, rolling back")
+            con.rollback()
+
+    finally:
+        if con:
+            con.close()
+
 
     con.close()
 
